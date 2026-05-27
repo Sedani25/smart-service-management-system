c4601940dad1db7813c8024f9120619802ddd281
SedaniLesara <sedanilesara25@gmail.com>
2026-05-27 14:56:06 +0530

    Added dynamic service management using JavaScript

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,33 @@
-function showMessage() {
-    alert("Welcome to the project!");
+let services = [];
+
+function addService() {
+
+    let input = document.getElementById("serviceInput");
+
+    let serviceName = input.value;
+
+    if(serviceName === "") {
+        alert("Please enter a service");
+        return;
+    }
+
+    services.push(serviceName);
+
+    displayServices();
+
+    input.value = "";
+}
+
+function displayServices() {
+
+    let list = document.getElementById("serviceList");
+
+    list.innerHTML = "";
+
+    for(let i = 0; i < services.length; i++) {
+
+        list.innerHTML += `
+            <li>${services[i]}</li>
+        `;
+    }
 }
