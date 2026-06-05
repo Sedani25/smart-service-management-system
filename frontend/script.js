bc42fcdce5aac84e1edcc46faf57ebb5409b7eae
SedaniLesara <sedanilesara25@gmail.com>
2026-06-05 23:51:35 +0530

    Added create service functionality

--- frontend/script.js
+++ frontend/script.js
@@ -1,36 +1,74 @@
-let loadButton =
-document.getElementById("loadButton");
+document
+    .getElementById("loadBtn")
+    .addEventListener("click", loadServices);
 
-loadButton.addEventListener(
-    "click",
-    loadServices
-);
+document
+    .getElementById("addBtn")
+    .addEventListener("click", addService);
+
+async function addService() {
+
+    const serviceName =
+        document.getElementById("serviceName").value;
+
+    const service = {
+
+        id: Date.now(),
+
+        name: serviceName,
+
+        status: "Pending"
+    };
+
+    await fetch(
+        "http://localhost:8080/services",
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type":
+                    "application/json"
+            },
+
+            body:
+                JSON.stringify(service)
+        }
+    );
+
+    document.getElementById(
+        "serviceName"
+    ).value = "";
+
+    loadServices();
+}
 
 async function loadServices() {
 
-    let response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos?_limit=5"
-    );
+    const response =
+        await fetch(
+            "http://localhost:8080/services"
+        );
 
-    let services = await response.json();
+    const services =
+        await response.json();
 
     displayServices(services);
 }
 
 function displayServices(services) {
 
-    let list =
-    document.getElementById("serviceList");
+    const list =
+        document.getElementById("serviceList");
 
     list.innerHTML = "";
 
     services.forEach(service => {
 
-        let li = document.createElement("li");
+        const li =
+            document.createElement("li");
 
-        li.innerHTML = `
-            ${service.title}
-        `;
+        li.textContent =
+            `${service.name} - ${service.status}`;
 
         list.appendChild(li);
     });
